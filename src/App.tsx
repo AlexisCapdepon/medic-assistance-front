b672ee5afe2e38b5fd7dcd07e1d20f3d2f3892dc
AlexisCapdepon <capdepon.alexis@gmail.com>
2021-03-03 23:30:29 +0100

    remove tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ function App() {
     <div className="App h-screen bg-gray-700">
       <header className="bg-gray-600 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">header</header>
       <body className="h-5/6">
-        
+        <div className="container mx-auto h-full flex flex-1 justify-center items-center">
+        <ul className="progressbar">
+          <li className="active">login</li>
+          <li>choose interest</li>
+          <li>add friends</li>
+          <li>View map</li>
+        </ul>
+        </div>
       </body>
       <footer>
         footer
