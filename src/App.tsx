aa3fd6a03d0096bfa180fbd790b7aa5036e90a99
AlexisCapdepon <capdepon.alexis@gmail.com>
2021-03-03 22:48:36 +0100

    init branch

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     <div className="App h-screen bg-gray-700">
       <header className="bg-gray-600 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">header</header>
       <body className="h-5/6">
-        body
+        
       </body>
       <footer>
         footer
